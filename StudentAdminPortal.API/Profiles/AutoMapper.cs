7ef07c5a7f4be771c203318ab8b25a97bd0c32b6
Goutham P Sudhir <Goutham.P.Sudhir@gds.ey.com>
2022-11-27 13:24:07 +0530

    End section 2

---
+++ StudentAdminPortal.API/Profiles/AutoMapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DataModels = StudentAdminPortal.API.DataModels;
+using DomainModels = StudentAdminPortal.API.DomainModels;
+
+namespace StudentAdminPortal.API.Profiles
+{
+    public class AutoMapper: Profile
+    {
+        public AutoMapper()
+        {
+            CreateMap<DomainModels.Student, DataModels.Student>().ReverseMap();
+
+            CreateMap<DomainModels.Gender, DataModels.Gender>().ReverseMap();
+
+            CreateMap<DomainModels.Address, DataModels.Address>().ReverseMap(); 
+        }
+    }
+}
